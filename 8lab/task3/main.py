db35014f9d3c91f221c0e4cc8b0fa718235df622
Michael <144711386+michaelmosin1@users.noreply.github.com>
2024-05-15 15:06:40 +0200

    Started to work on 8th lab

---
+++ 8lab/task3/main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+t = np.linspace(0, 14, 1000)
+plt.title('Вращение фигуры Лисажу')
+plt.xlabel('x')
+plt.ylabel('y')
+plt.grid()
+plt.plot(x, y)
+plt.show()
